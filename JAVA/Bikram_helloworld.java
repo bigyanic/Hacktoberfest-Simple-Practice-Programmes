ae74d7ddfe40df7d0826b9bd48f123fa23e58883
innatepanda <61987098+innatepanda@users.noreply.github.com>
2020-10-03 21:28:39 +0530

    Update Bikram_helloworld.java

--- JAVA/Bikram_helloworld.java
+++ JAVA/Bikram_helloworld.java
@@ -6,4 +6,9 @@ public class HelloWorld {
     public static void sayToPerson(String message, String name) {
         System.out.println(name + ", " + message);
     }
+    
+    public void( String name, String message, int date)
+    {
+        System.out.println(date+"/10/2020: "+name+" said "+message);
+    }
 }
