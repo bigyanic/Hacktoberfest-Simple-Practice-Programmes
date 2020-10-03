87bdec4a5e26576c45e63dc3990a37d3a3399e16
Bikram Parajuli <bikramparajuli0000@gmail.com>
2020-10-03 09:52:35 +0545

    Added some hello world

---
+++ JAVA/Bikram_helloworld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void say(String message) {
+        System.out.println(message);
+    }
+
+    public static void sayToPerson(String message, String name) {
+        System.out.println(name + ", " + message);
+    }
+}
---
+++ Javascript/Bikram_helloworld.js
@@ -0,0 +1,2 @@
+alert("Hello, world!");
+console.log("Hello world");
