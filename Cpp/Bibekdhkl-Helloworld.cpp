630841dedfe1c1ce824e08a6305ad0b7049f03b8
Bibek Dhakal <48583944+Bibekdhkl@users.noreply.github.com>
2020-10-04 19:32:11 +0545

    Added HelloWorld file in C++
    Added HelloWorld file in C++ with beginner friendly comments

---
+++ Cpp/Bibekdhkl-Helloworld.cpp
@@ -0,0 +1,8 @@
+#include<iostream>//iostream stands for standard input output stream
+
+using namespace std;//cin cout and alot of other things are defined in standard namespace
+
+int main(){
+  cout<<"Hello World";// cout displays any text inside quotation mark
+  return 0;//main function has return type integer
+  }
