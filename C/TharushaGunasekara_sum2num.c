de2b3d4aceaf9321e80d43bff735a725fd858e43
Tharusha Gunasekara <tharushagunasekara100@gmail.com>
2020-10-06 11:53:52 +0530

    sumoftwonumbers

---
+++ C/TharushaGunasekara_sum2num.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+   int num1, num2, sum;
+   printf("Enter First Number: ");
+   scanf("%d", &num1);
+   printf("Enter Second nnmber: ");
+   scanf("%d", &num2);
+
+   sum = num1 + num2;
+   printf("Sum of the two Entered Numbers: %d", sum);
+   return 0;
+}
