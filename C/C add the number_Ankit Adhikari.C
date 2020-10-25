fe302ccb9569d76e4a9d51d1a35d27b3d4137895
serialkille19 <73419596+serialkille19@users.noreply.github.com>
2020-10-25 13:39:05 +0545

    Create C add the number_Ankit Adhikari.C

---
+++ C/C add the number_Ankit Adhikari.C
@@ -0,0 +1,16 @@
+  
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  int a, b, c;
+
+  printf("Enter two numbers to add\n");
+  scanf("%d%d", &b, &c);
+
+  a = b + c;
+
+  printf("Sum of the numbers = %d\n", a);
+getch();
+  return 0;
+}
