20925eef2a407c235609f539afbe00271f12f703
Zuku271 <adrian.zuchowski@outlook.com>
2020-10-31 16:20:19 +0100

    Added files.

---
+++ C++/EulerMethod/eulerMethod.cpp
@@ -0,0 +1,9 @@
+constexpr double func(const double arg)
+{
+    return arg * arg;
+}
+
+double euler(const double x0, const double y0, const double h, double (*f)(const double))
+{
+    return 0;
+}
